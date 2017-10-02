c4e800d83b522dbaf851e19f051313158f869120
Louisa <louisaspicer@gmail.com>
2017-10-02 20:53:16 +0100

    Set up Angular frontend and Springboot backend with proxy config on port 4200

---
+++ api/src/main/java/com/louisa/explorePhilippines/ExplorePhilippinesApplication.java
@@ -0,0 +1,12 @@
+package com.louisa.explorePhilippines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorePhilippinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorePhilippinesApplication.class, args);
+	}
+}
